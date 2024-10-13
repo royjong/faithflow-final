9caf352eaf03eebccfbbdc2832a3dffecebaef29
Nova Slicer <roythedev.ai@gmail.com>
2024-10-13 18:49:01 +0200

    new appplication

--- app/dashboard/dashboardClient.tsx
+++ app/dashboard/dashboardClient.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 import { useSearchParams } from 'next/navigation';
 import { Button } from "@/components/ui/button";
 import { getDynamicIcon } from "@/utils/iconUtils";
